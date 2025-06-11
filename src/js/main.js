d383e77912ff81c587fb30d05be2f0f0f0646529
unknown <jarulcrip@gmail.com>
2025-06-11 18:46:05 +0300

    ready adaptive for header and hero

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,27 @@
-// FancyBox
+const mobileNavBtn = document.querySelector('.mobile-nav-btn');
+const mobileNavIcon = document.querySelector('.mobile-nav-btn__icon');
+const mobileNavPanel = document.querySelector('.mobile-nav__panel');
+
+mobileNavBtn.addEventListener('click', function () {
+	mobileNavIcon.classList.toggle('active');
+	mobileNavPanel.classList.toggle('visible');
+	document.body.classList.toggle('no-scroll');
+});
+
+function turnoffmobileNav() {
+	if (mobileNavBtn.classList.contains('active')) {
+		mobileNavBtn.classList.remove('active');
+	}
+	if (mobileNavPanel.classList.contains('visible')) {
+		mobileNavPanel.classList.remove('visible');
+	}
+	if (mobileNavIcon.classList.contains('active')) {
+		mobileNavIcon.classList.remove('active');
+	}
+}
+
+mobileNavPanel.querySelectorAll('a').forEach(function (link) {
+	link.addEventListener('click', function () {
+		turnoffmobileNav();
+	});
+});
